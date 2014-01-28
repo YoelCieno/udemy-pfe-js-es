e8f6b8858ef2954c3eeb0ac226131ab52c520284
YoelCieno <frmonteira@gmail.com>
2014-01-28 00:03:08 -0500

    Final de JS Básico- Proyecto final
    Busqueda final con condicional

---
+++ 34-html por cada usuario/js/eventos.js
@@ -0,0 +1,92 @@
+var usuarios = 
+[
+	{
+		nombre: 'Lucy',
+		genero: 'F',
+		aficion: 'pets',
+		avatar: 'avatar1.png'
+	},
+	{
+		nombre: 'Betty',
+		genero: 'F',
+		aficion: 'pets',
+		avatar: 'avatar2.png'
+	},
+	{
+		nombre: 'Ronald',
+		genero: 'M',
+		aficion: 'music',
+		avatar: 'avatar3.png'
+	},
+	{
+		nombre: 'Christopher',
+		genero: 'M',
+		aficion: 'sports',
+		avatar: 'avatar4.png'
+	},
+	{
+		nombre: 'Ximena',
+		genero: 'F',
+		aficion: 'reading',
+		avatar: 'avatar5.png'
+	},
+	{
+		nombre: 'Paul',
+		genero: 'M',
+		aficion: 'shopping',
+		avatar: 'avatar6.png'
+	},
+	{
+		nombre: 'Charlie',
+		genero: 'M',
+		aficion: 'pets',
+		avatar: 'avatar7.png'
+	},					
+];
+
+//si queremos seleccionar la ventana del navegador
+window.addEventListener('load', function()
+{
+	function buscar()
+	{
+		resultados.innerHTML = 'Hola, que tal<br/>\
+		¿Todo bien? <div style="background-color:#f16f51;">\
+		Espero que todo bien</div>';
+		
+		//conseguir hobby o afición	
+		var aficionCampo = document.getElementById('aficion');
+		var aficion = aficionCampo.value;
+		console.log(aficion);
+
+		//conseguir género
+		var generoCampo = document.getElementById('genero');
+		var selected = generoCampo.selectedIndex;
+		var genero = generoCampo.options[selected].value;
+		/*(Otra opción igual)
+		var genero = generoCampo.options[generoCampo.selectedIndex].value;
+		*/
+		console.log(genero);
+
+
+		var resultadosHTML = '';
+		var numUsuarios = usuarios.length;
+
+		for(var i = 0; i < numUsuarios; i++)
+		{
+			resultadosHTML += '<div class="personal">\
+												<img src="images/' + usuarios[i].avatar + '" />\
+												<div class="person-info">\
+												<div>'+ usuarios[i].nombre + '</div>\
+												<div>'+ usuarios[i].aficion + '</div></div>\
+													<button>Add as friend</button></div>';
+		}
+		resultados.innerHTML = resultadosHTML;			
+	};
+	/*Si queremos seleccionar un elemento de la página
+	lo haremos con document*/
+	var resultados = document.getElementById('resultados');	
+	var btn_buscar = document.getElementById('btn_buscar');
+	btn_buscar.addEventListener('click', buscar);
+	//Ahora vamos a hacer que busque antes de pulsar buscar
+	buscar();
+});
